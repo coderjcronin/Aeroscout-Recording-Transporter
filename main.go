59496fcf6e7eb8bcfc96c8618a0e2befac2c0f04
Josh C <coderjmc@gmail.com>
2025-03-06 17:12:14 -0500

    Sanity check work

--- main.go
+++ main.go
@@ -29,7 +29,56 @@ func mainMenu() int {
 	return 0
 }
 
+/*
+	sanityCheck() - returns boolean based on environment values
+
+sanityCheck will check that:
+1. We're in a recording session folder (there's a child folder with MAPPlaybackFile)
+__ I may need to disable this during development. __
+2. That we can create a file
+3. That we can delete the file we created
+
+If all 3 are valid, then return true. Otherwise, either we need this utility moved or admin rights and let the user know.
+*/
+func sanityCheck() bool {
+
+	entries, err := os.ReadDir("./") // Pull the file listing for the local directory
+
+	// If there was an error, log (well, print) it and return false
+	if err != nil {
+		fmt.Print(err)
+		return false
+	}
+
+	// Iterate through the listing for a directory
+	for _, file := range entries {
+		if file.IsDir() {
+			tempPath := "./" + file.Name()
+			tempListing, err := os.ReadDir(tempPath)
+			if err != nil {
+				fmt.Print(err)
+				return false
+			}
+			for _, subFile := range tempListing {
+				if subFile.Name() == "MAPPlaybackFile.dat" {
+					break // This isn't right... I need to set something then break so I can break the other loop and avoid the default false. Whoops.
+				}
+			}
+		}
+	} // If we found nothing, there's nothing to find and let the default return false
+
+	return false
+}
+
 func main() {
+
+	// Sanity check - check the first child directory to see if we're in a recording session.
+	weGood := sanityCheck()
+
+	if !weGood {
+		fmt.Println("Exiting.")
+		return
+	}
 
 	// Go "while True" loop. Will exit when menuSelection is invalid
 	for {
@@ -40,7 +89,7 @@ func main() {
 		} else if menuSelection == 2 {
 			prepAnalysis() // User selected to prep analysis, run function
 		} else {
-			break // Leave the loop
+			return // Leave the loop and function, we're done
 		}
 	}
 }
